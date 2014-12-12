703d5644ed7010380b1f95b0667a708708e321bb
AnatolyRugalev <anatoly.rugalev@gmail.com>
2014-12-12 13:17:39 +0500

    InvalidateNotification: ajax support

--- actions/InvalidateNotification.php
+++ actions/InvalidateNotification.php
@@ -1,27 +1,32 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Rugalev
  * Date: 27.10.2014
  * Time: 19:11
  */
-
-class InvalidateNotification extends CAction {
+class InvalidateNotification extends CAction
+{
 
 	public $return;
 
-	public function run($id = null) {
+	public function run($id = null)
+	{
 		$userId = Yii::app()->user->id;
-		if($id === null)
+		if ($id === null)
 			Notification::invalidateAll($userId);
 		else {
 			$notification = Notification::model()->findByAttributes(array('user_id' => $userId, 'id' => $id));
 			if ($notification !== null)
 				Notification::invalidate($id);
 		}
-		if(!isset($this->return))
+		if (!isset($this->return))
 			$this->return = array('/site/index');
-		$this->controller->redirect($this->return);
+		if (Yii::app()->request->isAjaxRequest)
+			die(json_encode(array('success' => true)));
+		else
+			$this->controller->redirect($this->return);
 	}
 
 }
